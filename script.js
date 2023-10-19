badc2638f1d90eb46c11a0cccb1824a857a63dec
ohdavir <davi.rosa.info@gmail.com>
2023-10-19 10:21:31 -0300

    refazendo o site

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
